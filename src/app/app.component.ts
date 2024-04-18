ffe7577d6aad31841af074afbf073554e1c07b05
Vipin-PE <vipinpe327@gmail.com>
2024-04-18 16:17:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sweetalert';
+
+  showAlert() {
+    Swal.fire({
+      title: "Hello there!",
+      text: "How is you ?",
+      icon: "success",
+      customClass: {
+        popup: 'custom-popup',
+      },
+      confirmButtonColor: 'rgb(33, 187, 33)'
+    });
+  }
 }
